0f2b20022a42e21121f2032afe228edcb3b6d401
pknowledge <abhishekrawane1195@gmail.com>
2021-12-23 07:43:52 +0530

    Added :  InvoiceGenerator class for caluculating cab fare.

---
+++ src/com/bridgelabz/CabInvoiceGenerator/WelcomeProgram.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.CabInvoiceGenerator;
+
+public class WelcomeProgram 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome Cab Invoice Generator Computation Problem");
+	}
+}
---
+++ src/com/bridgelabz/Test/CabInvoiceGeneratorTest.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.Test;
+
+import org.junit.Test;
+
+import com.bridgelabz.CabInvoiceGenerator.CabInvoiceGenerator;
+
+import junit.framework.Assert;
+
+public class CabInvoiceGeneratorTest 
+{
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare()
+	{
+		CabInvoiceGenerator cig = new CabInvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = cig.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
+	}
+	
+	@Test
+	public void givenLessDistanceOrTime_ShouldReturnMinimumFare()
+	{
+		CabInvoiceGenerator cig = new CabInvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = cig.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+}
